00b2db1878109e2110a8cb612f0db194d1b88aa2
mahedi hasan bijoy <bijoycse2014@gmail.com>
2023-02-13 09:33:25 +0600

    assignment3 modified and submitted

--- assignment3.php
+++ assignment3.php
@@ -3,7 +3,7 @@
  * 1. Write a Reusable  PHP Function that can check Even & Odd Number And Return Decision
  */
 
-function evenOrOdd(int $n):int {
+function evenOrOdd(int $n){
     if($n%2 == 0){
         return "even";
     }
@@ -13,8 +13,14 @@ function evenOrOdd(int $n):int {
 echo evenOrOdd(5);
 echo "\n";
 echo evenOrOdd(12);
-
+echo "\n";
 
  /**
   * 2. 1+2+3...…….100  Write a loop to calculate the summation of the series
   */
+
+$sum = 0;
+for($i=1; $i<=100; $i++){
+    $sum += $i;
+}
+echo $sum;
